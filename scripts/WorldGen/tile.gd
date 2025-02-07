74fb486e6da5992cc56acaf551d2abb6c8f819be
ForlornU <101473036+ForlornU@users.noreply.github.com>
2025-02-07 15:48:07 +0100

    tile neighbors
    Tile now knows its neighbors and if its a hill or not

--- scripts/WorldGen/tile.gd
+++ scripts/WorldGen/tile.gd
@@ -4,7 +4,7 @@ class_name Tile
 enum biome_type {Grassland, Forest, Desert, Ocean, Tundra, Mountain}
 var mesh_data : TileMeshData
 var pos_data : PositionData
-var neighbors = [Tile]
+var neighbors = []#Array[Tile]
 var occupier : Unit
 var debug_label : Label3D
 var biome : String
