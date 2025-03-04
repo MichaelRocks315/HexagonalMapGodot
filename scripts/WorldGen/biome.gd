60b7b01156af2690db55f8cbb66587ec4971509c
ForlornU <101473036+ForlornU@users.noreply.github.com>
2025-03-04 17:30:33 +0100

    Cleanup
    Remove all tile-related resources

--- scripts/WorldGen/biome.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name Biome
-
-@export var tile : TileMeshData
-@export var weight : float
--- scripts/WorldGen/position_data.gd
+++
@@ -1,8 +0,0 @@
-class_name PositionData
-
-var grid_position : Vector3i
-var world_position : Vector3
-var noise : float
-var buffer : bool = false
-var water : bool = false
-var hill : bool = false
