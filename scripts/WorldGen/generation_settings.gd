36e1ec2a9bab0b53e9c2c07c5c574c584d310a90
ForlornU <101473036+ForlornU@users.noreply.github.com>
2025-03-12 19:23:59 +0100

    adjusted size values

--- scripts/WorldGen/generation_settings.gd
+++ scripts/WorldGen/generation_settings.gd
@@ -6,9 +6,9 @@ enum shape {HEXAGONAL, RECTANGULAR, DIAMOND, CIRCLE}
 @export_category("Map")
 @export var map_shape : shape = shape.HEXAGONAL
 @export var map_seed : int
-@export_range(0, 99, 1) var radius: int = 5
-@export_range(1, 30, 1) var max_height: int = 3
-@export_range(0, 4) var terrace_steps = 1
+@export_range(0, 64, 1) var radius: int = 5
+@export_range(1, 32, 1) var max_height: int = 3
+@export_range(0, 6) var terrace_steps = 1
 @export_range(0, 1) var noise_strength : float = 0.5
 @export var flat_buffer = true
 @export var debug : bool = false
